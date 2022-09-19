4a162769ac5855064fb07d7961583bff4b815bdc
ewyser <manuwyser@gmail.com>
2022-09-19 15:16:27 +0200

    deleted permutedims for arrays

--- src/fun_D/flux/fluxes_D.jl
+++ src/fun_D/flux/fluxes_D.jl
@@ -95,3 +95,110 @@
     end    
     return nothing
 end
+
+
+
+
+
+
+# Hydrostatic reconstruction for variable topography
+@views function fluxRus2_D(UFS,U,z,g,nx,ny,dim) # see well-balanced scheme, e.g., http://www.lmm.jussieu.fr/~lagree/COURS/MFEnv/code_C_saintvenant.pdf
+    # index initialization
+    i  = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    j  = (blockIdx().y-1) * blockDim().y + threadIdx().y
+    if dim == 1#"F"
+        if i<=nx && j<=ny
+            # well-balanced scheme
+            hL = max(0.0,U[i,j,1]+z[i,j]-max(z[i,j],z[i+1,j]))
+            if hL > 0.0
+                UFS[i,j,1,1] = hL
+                UFS[i,j,2,1] = hL*U[i,j,2]/U[i,j,1]
+                UFS[i,j,3,1] = hL*U[i,j,3]/U[i,j,1]
+                UFS[i,j,1,3] = UFS[i,j,2,1]
+                UFS[i,j,2,3] = hL*(UFS[i,j,2,1]/UFS[i,j,1,1])^2+0.5*g*UFS[i,j,1,1]^2
+                UFS[i,j,3,3] = hL*(UFS[i,j,2,1]/UFS[i,j,1,1])*(UFS[i,j,3,1]/UFS[i,j,1,1])
+            end
+            hR  = max(0.0,U[i+1,j,1]+z[i+1,j]-max(z[i,j],z[i+1,j]))
+            if hR > 0.0
+                UFS[i,j,1,2] = hR 
+                UFS[i,j,2,2] = hR*U[i+1,j,2]/U[i+1,j,1]
+                UFS[i,j,3,2] = hR*U[i+1,j,3]/U[i+1,j,1]
+                UFS[i,j,1,4] = UFS[i,j,2,2]
+                UFS[i,j,2,4] = hR*(UFS[i,j,2,2]/UFS[i,j,1,2])^2+0.5*g*UFS[i,j,1,2]^2
+                UFS[i,j,3,4] = hR*(UFS[i,j,2,2]/UFS[i,j,1,2])*(UFS[i,j,3,2]/UFS[i,j,1,2])
+            end
+            UFS[i,j,2,5] = 0.5*g*(U[i+1,j,1]^2-hR^2)
+            UFS[i,j,2,6] = 0.5*g*(U[i  ,j,1]^2-hL^2)
+        end
+        # Rusanov flux definition
+        if i<=nx+1 && j<=ny
+            hL    = max(0.0,UFS[i,j,1,1])
+            if hL>0.0
+                uL = UFS[i,j,2,1]/hL
+            else
+                uL = 0.0
+            end
+            hR    = max(0.0,UFS[i,j,1,2])
+            if hR>0.0
+                uR = UFS[i,j,2,2]/hR
+            else
+                uR = 0.0
+            end
+            cL    = sqrt(g*hL)
+            cR    = sqrt(g*hR)
+            λ     = max(abs(uL-sqrt(g*hL)),abs(uR+sqrt(g*hR)))
+            for dim ∈ 1:3
+                UFS[i,j,dim,7] = (0.5*(UFS[i,j,dim,3]+UFS[i,j,dim,4])-0.5*λ*(UFS[i,j,dim,2]-UFS[i,j,dim,1]))
+            end
+        end
+
+
+
+
+    elseif dim == 2#"G"
+        if i<=nx && j<=ny
+            hL = max(0.0,U[i,j,1]+z[i,j]-max(z[i,j],z[i,j+1]))
+            if hL > 0.0
+                UFS[i,j,1,1] = hL
+                UFS[i,j,2,1] = hL*U[i,j,2]/U[i,j,1]
+                UFS[i,j,3,1] = hL*U[i,j,3]/U[i,j,1]
+                UFS[i,j,1,3] = UFS[i,j,3,1]
+                UFS[i,j,2,3] = hL*(UFS[i,j,2,1]/UFS[i,j,1,1])*(UFS[i,j,3]/UFS[i,j,1,1])
+                UFS[i,j,3,3] = hL*(UFS[i,j,3,1]/UFS[i,j,1,1])^2+0.5*g*UFS[i,j,1,1]^2
+            end
+            hR  = max(0.0,U[i,j+1,1]+z[i,j+1]-max(z[i,j],z[i,j+1]))
+            if hR > 0.0
+                UFS[i,j,1,2] = hR 
+                UFS[i,j,2,2] = hR*U[i,j+1,2]/U[i,j+1,1]
+                UFS[i,j,3,2] = hR*U[i,j+1,3]/U[i,j+1,1]
+                UFS[i,j,1,4] = UFS[i,j,3,2]
+                UFS[i,j,2,4] = hR*(UFS[i,j,2,2]/UFS[i,j,1,2])*(UFS[i,j,3,2]/UFS[i,j,1,2])
+                UFS[i,j,3,4] = hR*(UFS[i,j,3,2]/UFS[i,j,1,2])^2+0.5*g*UFS[i,j,1,2]^2
+            end
+            UFS[i,j,3,5] = 0.5*g*(U[i,j+1,1]^2-hR^2)
+            UFS[i,j,3,6] = 0.5*g*(U[i  ,j,1]^2-hL^2)
+        end
+        # Rusanov flux definition
+        if i<=nx && j<=ny+1
+            hL    = max(0.0,UFS[i,j,1,1])
+            if hL>0.0
+                uL = UFS[i,j,3,1]/hL
+            else
+                uL = 0.0
+            end
+            hR    = max(0.0,UFS[i,j,1,2])
+            if hR>0.0
+                uR = UFS[i,j,3,2]/hR
+            else
+                uR = 0.0
+            end
+            cL    = sqrt(g*hL)
+            cR    = sqrt(g*hR)
+            λ     = max(abs(uL-sqrt(g*hL)),abs(uR+sqrt(g*hR)))
+            for dim ∈ 1:3
+                UFS[i,j,dim,7] = (0.5*(UFS[i,j,dim,3]+UFS[i,j,dim,4])-0.5*λ*(UFS[i,j,dim,2]-UFS[i,j,dim,1]))
+            end
+    end
+    end    
+    return nothing
+end
--- src/fun_D/solve/advSolve_D.jl
+++ src/fun_D/solve/advSolve_D.jl
@@ -4,36 +4,26 @@ include("../bc/getBCs_D.jl")
 include("../flux/fluxes_D.jl")
 include("../upd/update_D.jl")
 @views function advSolve_D(cublocks,cuthreads,h_D,Qx_D,Qy_D,UFS,Ubc_D,U_D,zbc_D,z_D,g,Δx,Δy,Δt,nx,ny,type)
+    # x-direction, F flux vector
     # assembly of conservative variables vector and flux function vector
         @cuda blocks=cublocks threads=cuthreads getU_D(U_D,h_D,Qx_D,Qy_D,nx,ny)
         @cuda blocks=cublocks threads=cuthreads setUFS_D(UFS,nx+1,ny+1)
     # ghost cells
         @cuda blocks=cublocks threads=cuthreads getBC_D(zbc_D,Ubc_D,z_D,U_D,nx,ny,1)
-    # get fluxes
-        @cuda blocks=cublocks threads=cuthreads fluxRus_D(UFS,Ubc_D,zbc_D,g,nx,ny,1) 
+    # get fluxes x-direction
+        @cuda blocks=cublocks threads=cuthreads fluxRus2_D(UFS,Ubc_D,zbc_D,g,nx,ny,1) 
     # update along x-direction
         @cuda blocks=cublocks threads=cuthreads updateU_D(U_D,UFS,(Δt/Δx),nx,ny,1)
     synchronize()
 
+    # y-direction, G flux vector
     # ghost cells
-    U_D   = permutedims(U_D  ,(2,1,3))
-    z_D   = permutedims(z_D  ,(2,1)  )
-    Ubc_D = permutedims(Ubc_D,(2,1,3))
-    zbc_D = permutedims(zbc_D,(2,1)  )
-    @cuda blocks=cublocks threads=cuthreads setUFS_D(UFS,nx+1,ny+1)
-    synchronize()
-    UFS   = permutedims(UFS ,(2,1,3,4))
-    @cuda blocks=cublocks threads=cuthreads getBC_D(zbc_D,Ubc_D,z_D,U_D,ny,nx,1)
-    synchronize()
-
-    @cuda blocks=cublocks threads=cuthreads fluxRus_D(UFS,Ubc_D,zbc_D,g,ny,nx,2) 
-    synchronize()
-
-    UFS = permutedims(UFS ,(2,1,3,4))
-    U_D = permutedims(U_D ,(2,1,3))
-    z_D = permutedims(z_D ,(2,1)  )
-    @cuda blocks=cublocks threads=cuthreads updateU_D(U_D,UFS,(Δt/Δy),nx,ny,2)
-    synchronize()
-    @cuda blocks=cublocks threads=cuthreads getQxQyh_D(h_D,Qx_D,Qy_D,U_D,g,nx,ny)
+        @cuda blocks=cublocks threads=cuthreads setUFS_D(UFS,nx+1,ny+1)
+        @cuda blocks=cublocks threads=cuthreads getBC_D(zbc_D,Ubc_D,z_D,U_D,nx,ny,2)
+    # get fluxes y-direction
+        @cuda blocks=cublocks threads=cuthreads fluxRus2_D(UFS,Ubc_D,zbc_D,g,nx,ny,2) 
+    # update along y-direction
+        @cuda blocks=cublocks threads=cuthreads updateU_D(U_D,UFS,(Δt/Δy),nx,ny,2)
+        @cuda blocks=cublocks threads=cuthreads getQxQyh_D(h_D,Qx_D,Qy_D,U_D,g,nx,ny)
     synchronize()
 end
