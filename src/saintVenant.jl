5b2a5cfc57a45df3780d640a6192668df836f68e
ewyser <manuwyser@gmail.com>
2022-09-22 11:24:51 +0200

    add runoff_D() in saintVenant module

--- src/saintVenant.jl
+++ src/saintVenant.jl
@@ -1,41 +1,49 @@
 module saintVenant
-export geoflow,runoff,coast,basin
-export geoflow_D
+export geoflow,runoff,coast,basin # host code
+export geoflow_D,runoff_D # device code
 # include dependencies & function call(s)
-include(joinpath("./fun"  , "superInclude.jl"  )) # standard dependencies
-include(joinpath("./fun_D", "superInclude_D.jl")) # additional dependencies for Device & GPU computing
-global path_plot = "viz/out/"
+    include(joinpath("./fun"  , "superInclude.jl"  )) # standard dependencies
+    include(joinpath("./fun_D", "superInclude_D.jl")) # additional dependencies for Device & GPU computing
+# initialize & generate outputs and data folders
+    global path_plot = "viz/out/"
     if isdir(path_plot)==false
         mkdir(path_plot)    
     end
-global path_save = "viz/dat/"
+    global path_save = "viz/dat/"
     if isdir(path_save)==false
         mkdir(path_save)    
     end
-@info path_plot*" and "*path_save*" path generated..."    
+    @info path_plot*" and "*path_save*" path generated..."    
 # include geoflow routine in saintVenant module
-@doc raw"""
-    geoflow(lx::Float64,ly::Float64,nx::Int64,rheoType::String,solvType::String): solves a non-linear hyperbolic 2D Saint-Venant problem considering a Coulomb-type rheology within a finite volume framework on a Cartesian grid
-    # args:
-    - lx       : dimension along the x-direciton.
-    - ly       : dimension along the y-direciton.
-    - nx       : number of grid nodes along the x-direction.
-    - rheoType : select the rheology, i.e., "coulomb", "newtonian" or "plastic"
-    - solveType: select the numerical flux, i.e., "Rusanov", "HLL" or "HLLC"
-    To run geoflow() on a GPU, add _D, i.e., geoflow_D(lx::Float64,ly::Float64,nx::Int64,rheoType::String,solvType::String)
-"""
-geoflow()
-include(joinpath("../scripts", "geoflow.jl"))
-@info "(✓) geoflow()"
+    @doc raw"""
+        geoflow(lx::Float64,ly::Float64,nx::Int64,rheoType::String,solvType::String): solves a non-linear hyperbolic 2D Saint-Venant problem considering a Coulomb-type rheology within a finite volume framework on a Cartesian grid
+        # args:
+        - lx       : dimension along the x-direciton.
+        - ly       : dimension along the y-direciton.
+        - nx       : number of grid nodes along the x-direction.
+        - rheoType : select the rheology, i.e., "coulomb", "newtonian" or "plastic"
+        - solveType: select the numerical flux, i.e., "Rusanov", "HLL" or "HLLC"
+        To run geoflow() on a GPU, add _D, i.e., geoflow_D(lx::Float64,ly::Float64,nx::Int64,rheoType::String,solvType::String)
+    """
+    geoflow()
+    include(joinpath("../scripts", "geoflow.jl"))
 # include runoff routine in saintVenant module
-include(joinpath("../scripts", "runoff.jl"))
-@info "(✓) runoff()"
+    @doc raw"""
+        runoff(path::String): solves a non-linear hyperbolic 2D Saint-Venant problem considering a Newtonian-type rheology within a finite volume framework on a Cartesian grid
+        # args:
+        - path     : path (absolute or relative) to a DSM/DTM/DEM in .asc format
+        On Windows OS, use "/" instead of "\"
+    """
+    runoff()
+    @info "(✓) geoflow(), geoflow_D()"
+    include(joinpath("../scripts", "runoff.jl"))
+    @info "(✓) runoff() , runoff_D()"
 # include coast routine in saintVenant module
-include(joinpath("../scripts", "coast.jl"))
-@info "(✓) coast()"
+    include(joinpath("../scripts", "coast.jl"))
+    @info "(✓) coast()"
 # include basin routine in saintVenant module
-include(joinpath("../scripts", "basin.jl"))
-@info "(✓) basin()"
+    include(joinpath("../scripts", "basin.jl"))
+    @info "(✓) basin()"
 end
 
 #=
